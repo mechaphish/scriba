23a6e57e59f6a5d9556fcb03b621c71771f927c7
Yan <yans@yancomm.net>
2016-07-27 18:47:49 -0700

    update the testcase

--- tests/test_cb.py
+++ tests/test_cb.py
@@ -200,80 +200,83 @@ class TestCBSubmitter():
         cbn_p1 = CBN.create(cs=cs, name="patch1", blob="XXXYZ", patch_type=pt1)
         pt2 = PT.create(name="b_patch", functionality_risk=0., exploitability=0.3)
         cbn_p2 = CBN.create(cs=cs, name="patch2", blob="XXXZZ", patch_type=pt2)
+
+        # Make sure we grab the "best" patch
+        assert_items_equal(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs), [cbn_p2])
+
+        # emulate the ambassador submitting this fucker
+        csf0s = CSF.create(cs=cs, cbns=[cbn_p2], team=t, submission_round=r0)
+
+        # add a new, better patch
         pt3 = PT.create(name="c_patch", functionality_risk=0., exploitability=0.2)
         cbn_p3 = CBN.create(cs=cs, name="patch3", blob="XXXXZ", patch_type=pt3)
+
+        # make sure we select the new patch, because it's still the right round
+        assert_items_equal(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs), [cbn_p3])
+        csf0s.cbns = [cbn_p3]
+        csf0s.save()
+
+        # make sure we are now happy
+        assert_is_none(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs))
+
+        # down a round
+        r1 = Round.create(num=1)
+        pf_r1 = PF.create(
+            cs=cs, round=r1,
+            success=0, timeout=0, connect=0, function=0,
+            time_overhead=0.0, memory_overhead=0.0
+        )
+        CSF.create(cs=cs, cbns=[cbn_p3], team=t, available_round=r1, poll_feedback=pf_r1)
+
+        # tick the round
+        r2 = Round.create(num=2)
+        pf_r2 = PF.create(
+            cs=cs, round=r2,
+            success=1.0, timeout=0, connect=0, function=0,
+            time_overhead=0.05, memory_overhead=0.05
+        )
+        CSF.create(cs=cs, cbns=[cbn_p3], team=t, available_round=r2, poll_feedback=pf_r2)
+
+        # make sure we don't choose to change the selection
+        assert_is_none(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs))
+
+        # now find a better patch
         pt4 = PT.create(name="d_patch", functionality_risk=0., exploitability=0.1)
         cbn_p4 = CBN.create(cs=cs, name="patch4", blob="XXXYX", patch_type=pt4)
 
-        # Make sure we properly handle the case when feedback is missing
+        # too late, man
         assert_is_none(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs))
 
-        # now the patch score comes in for the first one
-        PS.create(
-            cs=cs, patch_type=pt1, round=r0,
-            num_polls=10, has_failed_polls=True, failed_polls=1,
-            perf_score={
-                'score': {
-                    'ref': { 'task_clock': 1.0, 'rss': 1.0, 'flt': 1.0, 'file_size': 1.0 },
-                    'rep': { 'task_clock': 1.1, 'file_size': 1.1, 'rss': 1.1, 'flt': 1.1 }
-                }
-            }
+        # now we get the baaaad news
+        r3 = Round.create(num=3)
+        pf_r3 = PF.create(
+            cs=cs, round=r3,
+            success=0.8, timeout=0, connect=0, function=0.2,
+            time_overhead=0.05, memory_overhead=0.05
         )
-
-        # Make sure we don't choose that
-        assert_is_none(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs))
-
-        # two more test results come in
-        PS.create(
-            cs=cs, patch_type=pt2, round=r0,
-            num_polls=10, has_failed_polls=False, failed_polls=0,
-            perf_score={
-                'score': {
-                    'ref': { 'task_clock': 1.0, 'rss': 1.0, 'flt': 1.0, 'file_size': 1.0 },
-                    'rep': { 'task_clock': 1.1, 'file_size': 1.1, 'rss': 1.1, 'flt': 1.1 }
-                }
-            }
-        )
-        PS.create(
-            cs=cs, patch_type=pt3, round=r0,
-            num_polls=10, has_failed_polls=False, failed_polls=0,
-            perf_score={
-                'score': {
-                    'ref': { 'task_clock': 1.0, 'rss': 1.0, 'flt': 1.0, 'file_size': 1.0 },
-                    'rep': { 'task_clock': 1.1, 'file_size': 1.1, 'rss': 1.1, 'flt': 1.1 }
-                }
-            }
-        )
-
-        # Make sure we choose the more secure one
-        chosen_1 = scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs)
-        assert_items_equal(chosen_1, [cbn_p3])
-
-        # now we get a real result, telling us that it's fucked
-        r2 = Round.create(num=2)
-        pf_3 = PF.create(
-            cs=cs, round=r2,
-            success=1.0, timeout=0, connect=0, function=0,
-            time_overhead=0.3, memory_overhead=0.3
-        )
-        CSF.create(cs=cs, cbns=chosen_1, team=t, available_round=r2, poll_feedback=pf_3)
+        CSF.create(cs=cs, cbns=[cbn_p3], team=t, available_round=r3, poll_feedback=pf_r3)
 
         # Make sure we properly roll back
         assert_items_equal(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs), cs.cbns_original)
-        CSF.create(cs=cs, cbns=cs.cbns_original, team=t, submission_round=r2)
+        CSF.create(cs=cs, cbns=cs.cbns_original, team=t, submission_round=r3)
 
-        # now we get the result for the third, awesome, patch
-        PS.create(
-            cs=cs, patch_type=pt4, round=r2,
-            num_polls=10, has_failed_polls=False, failed_polls=0,
-            perf_score={
-                'score': {
-                    'ref': { 'task_clock': 1.0, 'rss': 1.0, 'flt': 1.0, 'file_size': 1.0 },
-                    'rep': { 'task_clock': 1.1, 'file_size': 1.1, 'rss': 1.1, 'flt': 1.1 }
-                }
-            }
+        # down a round
+        r4 = Round.create(num=3)
+        pf_r4 = PF.create(
+            cs=cs, round=r4,
+            success=0, timeout=0, connect=0, function=0,
+            time_overhead=0.0, memory_overhead=0.0
         )
+        CSF.create(cs=cs, cbns=[cbn_orig], team=t, available_round=r4, poll_feedback=pf_r4)
 
-        # Make sure we don't thrash
-        chosen_last = scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs)
-        assert_is_none(chosen_last)
+        # that worked
+        r5 = Round.create(num=5)
+        pf_r5 = PF.create(
+            cs=cs, round=r1,
+            success=1.0, timeout=0, connect=0, function=0,
+            time_overhead=0.0, memory_overhead=0.0
+        )
+        CSF.create(cs=cs, cbns=[cbn_orig], team=t, available_round=r5, poll_feedback=pf_r5)
+
+        # make sure we're happy staying unpatched
+        assert_is_none(scriba.submitters.cb.CBSubmitter.patch_decision_simple(cs))
